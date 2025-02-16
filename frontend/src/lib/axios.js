e0f08b631b636ba575df51ed6e4b6212a8a8adf4
Sanchit <sanchitpatil81004@gmail.com>
2025-02-16 21:26:39 +0530

    Updated axios url for 2nd time
    Updated axios url for 2nd time

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.MODE === "development" ? "https://linkify-f7aq.onrender.com/api/v1" : "/api/v1",
+	baseURL: import.meta.env.MODE === "development" ? "https://linkify-7zdi.onrender.com/api/v1" : "/api/v1",
 	withCredentials: true,
 });
